ce62f34060b7a5a7940c34206054db33cd06d178
Fernando <fernandochable@ciencias.unam.mx>
2026-05-25 22:29:17 -0600

    se definieron los métodos eliminar vértice y arista

--- EDD/GraficaLista.java
+++ EDD/GraficaLista.java
@@ -24,7 +24,8 @@ public class GraficaLista<T> implements Grafica<T> {
         }
     }
 
-    private ListaDoblementeLigada<Vertice> vertices;
+
+    public ListaDoblementeLigada<Vertice> vertices;
     int numVertices;
     int numAristas;
 
@@ -198,10 +199,109 @@ public class GraficaLista<T> implements Grafica<T> {
 
     @Override
     public void eliminarVertice(T elemento) {
+
+        /*
+        COMENTARIO RÁPIDO: Se tuvo que cambiar el método buscar que sale en el algoritmo por estas líneas de código debido a que el método buscar
+        en los vertices de la gráfica espera que se le pase como parámetro un vértice, y el elemento es un objeto de tipo T, por lo que no lo
+        pudimos usar.
+         */
+
+        Vertice verticeAEliminar = null;
+        int kVertice = 0;
+
+        for(Vertice vertice : this.vertices) {
+            if(vertice.nombre.equals(elemento)) {
+                verticeAEliminar = vertice;
+                break;
+            }
+        }
+
+        if(verticeAEliminar == null) {
+            throw new IllegalArgumentException("El vértice no existe.");
+        }
+
+        for(Vertice actual : this.vertices) {
+            Adyacencia adyacencia = null;
+            int kAdyacencia = 0;
+            int indiceAdyacencia = -1;
+
+            for(Adyacencia ady : actual.adyacencias) {
+                if(ady.nombre.equals(elemento)) {
+                    adyacencia = ady;
+                    indiceAdyacencia = kAdyacencia;
+                    break;
+                }
+                kAdyacencia++;
+            }
+
+            if(adyacencia != null) {
+                actual.adyacencias.eliminar(indiceAdyacencia);
+                this.numAristas--;
+            }
+        }
+
+        kVertice = this.vertices.devolverIndiceElemento(verticeAEliminar);
+        this.vertices.eliminar(kVertice);
+        this.numVertices--;
     }
 
     @Override
     public void eliminarArista(T e1, T e2) {
+    
+        Vertice uVertice = null;
+        Vertice vVertice = null;
+    
+        int i = 0, j = 0;
+        int indiceU = -1;
+        int indiceV = -1;
+
+    
+        for (Vertice v : this.vertices) {
+            if (v.nombre.equals(e1)) {
+                uVertice = v;
+                indiceU = i;
+            }
+            if (v.nombre.equals(e2)) {
+                vVertice = v;
+                indiceV = j;
+            }
+            i++;
+            j++;
+        }
+
+        if (uVertice == null || vVertice == null) {
+            throw new IllegalArgumentException("Alguno o ambos extremos de la arista no existen.");
+        }
+
+        int k = -1;
+        int l = -1;
+    
+        int contador = 0;
+        for (Adyacencia actual : uVertice.adyacencias) {
+            if (actual.nombre.equals(e2)) {
+                k = contador;
+                break;
+            }
+            contador++;
+        }
+
+        contador = 0;
+        for (Adyacencia actual : vVertice.adyacencias) {
+            if (actual.nombre.equals(e1)) {
+                l = contador;
+                break;
+            }
+            contador++;
+        }
+
+        if (k == -1 || l == -1) {
+            throw new IllegalArgumentException("La arista entre ambos vértices no existe.");
+        }
+
+        uVertice.adyacencias.eliminar(k);
+        vVertice.adyacencias.eliminar(l);
+
+        this.numAristas--;
     }
 
     @Override
